8089574cdd0768c08e96110eb58730f66cd2da9d
Eometheous <jonathan.sthomas@icloud.com>
2024-04-09 14:25:50 -0700

    tweaked movement

--- Assets/Objects/Player/PlayerController.cs
+++ Assets/Objects/Player/PlayerController.cs
@@ -1,17 +1,12 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    private readonly float movementForce = 10f;
+    private readonly float groundSpeed = 5f;
     private readonly float airMovementForce = 4f;
-    private readonly float jumpVelocity = 140f;
+    private readonly float jumpForce = 250f;
     private readonly float jumpForceAir = 7f;
     public Rigidbody2D rb;
     private Vector2 movementDirection;
@@ -43,8 +38,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         movementDirection = new Vector2(Input.GetAxis("Horizontal"), 0);
-        if (Input.GetAxis("Vertical") > 0) jumping = true;
-        else jumping = false;
+        
+        jumping = Input.GetAxis("Vertical") > 0;
 
         if (rb.velocity.x < 0) GetComponent<SpriteRenderer>().flipX = true;
         else if (rb.velocity.x > 0) GetComponent<SpriteRenderer>().flipX = false;
@@ -59,10 +54,11 @@ public class PlayerController : MonoBehaviour
         
         if (touchingGround) 
         {
-            force += movementDirection * movementForce - rb.velocity;
-            if (jumping) force += Vector2.up * jumpVelocity;
+            // force += movementDirection * groundSpeed;
+            rb.velocity = movementDirection * groundSpeed;
+            if (jumping) force += Vector2.up * jumpForce;
         }
-        else force += movementDirection * airMovementForce - rb.velocity;
+        else force += movementDirection * airMovementForce;
         rb.AddForce(force);
         if (jumping && rb.velocity.y > 0) rb.AddForce(Vector2.up * jumpForceAir);
         
