42733c3a8129d9add8b72ac4561f6b6520bf016a
Eometheous <jonathan.sthomas@icloud.com>
2024-05-13 14:24:57 -0700

    added level 8

--- Assets/Objects/Tower/CameraSwitcher.cs
+++ Assets/Objects/Tower/CameraSwitcher.cs
@@ -6,19 +6,37 @@ public class CameraSwitcher : MonoBehaviour
 {
     public Camera camera1, camera2;
 
+    public bool updown = false;
+
     public void OnTriggerStay2D(Collider2D other) 
     {
         if (other.CompareTag("Player"))
         {
-            if (other.GetComponent<Rigidbody2D>().velocity.x > 0)
+            if (updown)
             {
-                camera1.gameObject.SetActive(false);
-                camera2.gameObject.SetActive(true);
+                if (other.GetComponent<Rigidbody2D>().velocity.y > .5)
+                {
+                    camera1.gameObject.SetActive(false);
+                    camera2.gameObject.SetActive(true);
+                }
+                else if (other.GetComponent<Rigidbody2D>().velocity.y < -.5)
+                {
+                    camera2.gameObject.SetActive(false);
+                    camera1.gameObject.SetActive(true);
+                }
             }
-            else if (other.GetComponent<Rigidbody2D>().velocity.x < 0)
+            else
             {
-                camera2.gameObject.SetActive(false);
-                camera1.gameObject.SetActive(true);
+                if (other.GetComponent<Rigidbody2D>().velocity.x > 0)
+                {
+                    camera1.gameObject.SetActive(false);
+                    camera2.gameObject.SetActive(true);
+                }
+                else if (other.GetComponent<Rigidbody2D>().velocity.x < 0)
+                {
+                    camera2.gameObject.SetActive(false);
+                    camera1.gameObject.SetActive(true);
+                }
             }
         }
     }
